2bc230c419cd11ff9d1d0a9642ac6236fa8ba7f5
David Losas <davidlosas93@gmail.com>
2025-04-09 14:01:19 +0200

    feat(card): implement change PIN controller with unit test

--- tests/unit/card/card.service.unit-spec.ts
+++ tests/unit/card/card.service.unit-spec.ts
@@ -8,16 +8,18 @@ import { CreateCardDto } from '@/modules/card/dto/create-card.dto';
 import { NotFoundException } from '@nestjs/common';
 import { ActivateCardDto } from '@/modules/card/dto/activate-card.dto';
 import { HashingService } from '@/modules/card/services/hashing-service';
-
-const activateCardDto: ActivateCardDto = {
-    pin: '1234',
-    repeatedPin: '1234',
-};
+import { ChangeCardPinDto } from '@/modules/card/dto/change-card-pin.dto';
 
 describe('CardService', () => {
     let service: CardService;
     let cardRepo: jest.Mocked<Repository<Card>>;
     let accountRepo: jest.Mocked<Repository<Account>>;
+    let hashingService: jest.Mocked<HashingService>;
+
+    const activateCardDto: ActivateCardDto = {
+        pin: '1234',
+        repeatedPin: '1234',
+    };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -51,38 +53,37 @@ describe('CardService', () => {
             ],
         }).compile();
 
-        service = module.get<CardService>(CardService);
+        service = module.get(CardService);
         cardRepo = module.get(getRepositoryToken(Card));
         accountRepo = module.get(getRepositoryToken(Account));
+        hashingService = module.get(HashingService);
     });
 
     it('debería crear y guardar una tarjeta correctamente', async () => {
-        const fakeAccount = {
-            id: '9586d894-9e6d-4fe7-959d-3889c8b171b1',
-            iban: 'ES7620770024003102575766',
+        const fakeAccount: Account = {
+            id: 'account-id',
+            iban: 'ES00',
             balance: '0',
             createdAt: new Date(),
-        } satisfies Account;
+        };
 
-        const fakeCard = {
-            id: '22f7c4e1-602f-4b88-9845-a47567029aae',
+        const fakeCard: Card = {
+            id: 'card-id',
             accountId: fakeAccount.id,
             account: fakeAccount,
-            number: '41111111111111111111',
+            number: '4111111111111111',
             pin: null,
             isActive: false,
             cardType: 'debit',
             withdrawalLimit: 1000,
             creditLimit: 0,
             createdAt: new Date(),
-        } satisfies Card;
+        };
 
         cardRepo.create.mockReturnValue(fakeCard);
         cardRepo.save.mockResolvedValue(fakeCard);
 
-        const createCardDto: CreateCardDto = {
-            cardType: 'debit',
-        };
+        const createCardDto: CreateCardDto = { cardType: 'debit' };
 
         const result = await service.create(createCardDto, fakeAccount.id);
 
@@ -102,26 +103,21 @@ describe('CardService', () => {
             mockQueryBuilder,
         );
 
-        const createCardDto: CreateCardDto = {
-            cardType: 'debit',
-        };
+        const createCardDto: CreateCardDto = { cardType: 'debit' };
 
         await expect(
-            service.create(
-                createCardDto,
-                '6f5925fc-7793-47bb-8690-a2b8e85a9fdc',
-            ),
+            service.create(createCardDto, 'non-existent-account-id'),
         ).rejects.toThrow(NotFoundException);
     });
 
     it('debería activar una tarjeta correctamente si está inactiva y sin PIN', async () => {
-        const cardId = '22f7c4e1-602f-4b88-9845-a47567029aae';
+        const cardId = 'card-id';
 
         const inactiveCard: Card = {
             id: cardId,
-            accountId: '17827f21-6d3a-4d54-9a28-06810eae84b6',
+            accountId: 'account-id',
             account: {} as Account,
-            number: '41111111111111111111',
+            number: '4111111111111111',
             pin: null,
             isActive: false,
             cardType: 'debit',
@@ -149,16 +145,89 @@ describe('CardService', () => {
     });
 
     it('debería lanzar error si la tarjeta ya está activa', async () => {
-        const cardId = 'a0d1e7b8-e3e8-48e3-a2eb-6c0cd02e87c6';
+        const cardId = 'card-id';
 
         cardRepo.findOneBy.mockResolvedValue({
             id: cardId,
-            pin: '1234',
+            pin: 'already-hashed',
             isActive: true,
         } as Card);
 
         await expect(service.activate(activateCardDto, cardId)).rejects.toThrow(
             'La tarjeta ya está activa',
+        );
+    });
+
+    it('debería cambiar correctamente el PIN si todo es válido', async () => {
+        const cardId = 'card-id';
+        const oldHashedPin = 'hashed-old';
+        const newHashedPin = 'hashed-new';
+
+        const card: Card = {
+            id: cardId,
+            isActive: true,
+            pin: oldHashedPin,
+        } as Card;
+
+        const changeCardPinDto: ChangeCardPinDto = {
+            currentPin: '1234',
+            newPin: '5678',
+            repeatedNewPin: '5678',
+        };
+
+        cardRepo.findOneBy.mockResolvedValue(card);
+        hashingService.compare
+            .mockResolvedValueOnce(false)
+            .mockResolvedValueOnce(true);
+        hashingService.hash.mockResolvedValue(newHashedPin);
+
+        await service.changeCardPin(changeCardPinDto, cardId);
+
+        expect(hashingService.compare).toHaveBeenCalledWith(
+            '5678',
+            oldHashedPin,
+        );
+        expect(hashingService.compare).toHaveBeenCalledWith(
+            '1234',
+            oldHashedPin,
+        );
+        expect(hashingService.hash).toHaveBeenCalledWith('5678');
+        expect(cardRepo.save).toHaveBeenCalledWith(
+            expect.objectContaining({ pin: newHashedPin }),
+        );
+    });
+
+    it('debería lanzar error si el PIN actual no coincide', async () => {
+        const cardId = 'card-id';
+        const oldHashedPin = 'hashed-old-pin';
+
+        const card: Card = {
+            id: cardId,
+            isActive: true,
+            pin: oldHashedPin,
+        } as Card;
+
+        const dto: ChangeCardPinDto = {
+            currentPin: '1234',
+            newPin: '5678',
+            repeatedNewPin: '5678',
+        };
+
+        cardRepo.findOneBy.mockResolvedValue(card);
+
+        hashingService.compare.mockResolvedValueOnce(false);
+
+        await expect(service.changeCardPin(dto, cardId)).rejects.toThrow(
+            'El PIN actual es incorrecto',
+        );
+
+        expect(hashingService.compare).toHaveBeenCalledWith(
+            '5678',
+            oldHashedPin,
+        );
+        expect(hashingService.compare).toHaveBeenCalledWith(
+            '1234',
+            oldHashedPin,
         );
     });
 });
